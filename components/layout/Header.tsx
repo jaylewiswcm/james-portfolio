c2207142fc2962ecf6132e608d7c6136887dfb41
wcmbristol <benjamin.kenny@worldclassmedia.co.uk>
2021-09-23 21:35:18 +0100

    Replacing a tags with next/link

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-
+import Link from 'next/link'
 const Header = () => {
     return (
         <header>
             <nav>
                 <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/projects">Projects</a></li>
-                    <li><a href="/gallery">Gallery</a></li>
-                    <li><a href="/about">About me</a></li>
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/projects">Projects</Link></li>
+                    <li><Link href="/gallery">Gallery</Link></li>
+                    <li><Link href="/about">About me</Link></li>
                     <li className="contact-link"><a href="/contact">Contact</a></li>
                 </ul>
             </nav>
