1cebd96d75f25ddcb56e73e8b3beae2841de3708
wcmbristol <benjamin.kenny@worldclassmedia.co.uk>
2021-09-26 21:30:21 +0100

    Added active links

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Footer = () => {
+    const router = useRouter();
+    
     return (
         <footer>
             <ul>
                 <li className="list-title">Navigation</li>
-                <li><Link href="/"><a>Portfolio</a></Link></li>
-                <li><Link href="/gallery"><a>Gallery</a></Link></li>
-                <li><Link href="/about"><a>About</a></Link></li>
-                <li><Link href="/contact"><a>Contact</a></Link></li>
+                <li><Link href="/"><a className={router.pathname.includes('portfolio') ? "active" : "" || router.pathname == "/" ? "active" : ""}>Portfolio</a></Link></li>
+                <li><Link href="/gallery"><a className={router.pathname == "/gallery" ? "active" : ""}>Gallery</a></Link></li>
+                <li><Link href="/about"><a className={router.pathname == "/about" ? "active" : ""}>About</a></Link></li>
+                <li><Link href="/contact"><a className={router.pathname == "/contact" ? "active" : ""}>Contact</a></Link></li>
             </ul>
             <ul>
                 <li className="list-title">Email</li>
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router';
+
 const Header = () => {
+    const router = useRouter();
+    
     return (
         <header>
             <h1 className="header-title">James Thompson-Gorwill / Print Designer </h1>
             <nav>
                 <ul>
                     {/* <li><Link href="/">Home</Link></li> */}
-                    <li><Link href="/"><a>Portfolio</a></Link></li>
-                    <li><Link href="/gallery"><a>Gallery</a></Link></li>
-                    <li><Link href="/about"><a>About</a></Link></li>
+                    <li><Link href="/"><a className={router.pathname.includes('portfolio') ? "active" : "" || router.pathname == "/" ? "active" : ""}>Portfolio</a></Link></li>
+                    <li><Link href="/gallery"><a className={router.pathname == "/gallery" ? "active" : ""}>Gallery</a></Link></li>
+                    <li><Link href="/about"><a className={router.pathname == "/about" ? "active" : ""}>About</a></Link></li>
                     {/* <li className="contact-link"><Link href="/contact">Contact</Link></li> */}
                 </ul>
                 <div className="contact-link">
-                    <Link href="/contact"><a>Contact</a></Link>
+                    <Link href="/contact"><a className={router.pathname == "/contact" ? "active" : ""}>Contact</a></Link>
                 </div>
             </nav>
         </header>
