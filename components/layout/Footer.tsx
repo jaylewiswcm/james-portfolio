abee3d984b110aa4dfcb31cb5afb4a2863543eb2
wcmbristol <benjamin.kenny@worldclassmedia.co.uk>
2021-09-29 08:46:27 +0100

    Making responsive

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -14,17 +14,20 @@ const Footer = () => {
                 <li><Link href="/about"><a className={router.pathname == "/about" ? "active" : ""}>About</a></Link></li>
                 <li><Link href="/contact"><a className={router.pathname == "/contact" ? "active" : ""}>Contact</a></Link></li>
             </ul>
-            <ul>
-                <li className="list-title">Email</li>
-                <li><Link href="mailto:jamesthomson.gorwill@live.co.uk"><a>Jamesthomson.gorwill@live.co.uk</a></Link></li>
-            </ul>
-            <ul>
-                <li className="list-title">Phone</li>
-                <li><Link href="tel:07943266481"><a>07943266481</a></Link></li>
-            </ul>
-            <ul>
-                <li className="list-title">Social</li>
-                <li><Link href="/"><a>Instagram</a></Link></li>
+
+            <ul className="footer-contact-wrapper">
+                <li>
+                    <p className="list-title">Email</p>
+                    <Link href="mailto:jamesthomson.gorwill@live.co.uk"><a>Jamesthomson.gorwill@live.co.uk</a></Link>
+                </li>
+                <li>
+                    <p className="list-title">Phone</p>
+                    <Link href="tel:07943266481"><a>07943266481</a></Link>
+                </li>
+                <li>
+                    <p className="list-title">Social</p>
+                    <Link href="/"><a>Instagram</a></Link>
+                </li>
             </ul>
         </footer>
     )
